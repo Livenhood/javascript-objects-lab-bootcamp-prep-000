9589825f756af2146652f95221b5002b932358eb
Michelle Livengood <ml2876@nyu.edu>
2018-02-08 16:05:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 
 function deleteFromObjectByKey(object, key) {
   const newObj = Object.assign({},object)
-  delete newObj.key 
+  delete newObj[key] 
   return newObj
 }
